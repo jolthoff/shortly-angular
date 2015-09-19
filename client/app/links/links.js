19dc24389c8b00df801c134851ef312ded7ada20
jolthoff <jl.olthoff@gmail.com>
2015-09-18 17:31:59 -0700

    Implement linksController.js

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,32 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.filterString = '';
+  //whitespace
+  $scope.allLinks = [];
+  //imagine the space
+  (function() { //fetch all links from mongoDB
+    //so pretty
+    $scope.allLinks = Links.fetch().sort(function(linkA, linkB) {
+
+      return linkA.visits - linkB.visits;
+      
+    });
+    //more whitespace
+  })();
+
+  $scope.filteredArray = function() {
+
+    return allLinks.filter(function(link) {
+
+      return link.title.test(new RegExp($scope.filterString)) || 
+
+      link.url.test(new RegExp($scope.filterString)) ||
+
+      link.link.test(new RegExp($scope.filterString));
+
+    });
+
+  };
+
 });
